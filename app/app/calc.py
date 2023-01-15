18cc0495ded2cc82179659dff559c0db59b640d8
Naren <narenkarthi@gmail.com>
2023-01-16 00:27:16 +0100

    added wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+'''calculator function'''
+
+
+def add(x, y):
+    """
+    add x and y and return the result
+
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """Substract x from y and retun result."""
+    return y - x
