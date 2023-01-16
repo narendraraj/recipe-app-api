a9aba4001c773ce6e73b7683c7a2a6e104d163ab
Naren <narenkarthi@gmail.com>
2023-01-16 04:28:09 +0100

    after lint corrections

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,12 +5,14 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
+
     def test_create_user_with_email_succesful(self):
         """Test creating a user with an email is succsesful."""
-        email = 'test@example.com'
-        password = 'testpass123'
+        email = "test@example.com"
+        password = "testpass123"
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
@@ -20,29 +22,28 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """ Test email is nomraliezed for new users."""
+        """Test email is nomraliezed for new users."""
 
         sample_email = [
-            ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
-            ['test4@example.com', 'test4@example.com']
-
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.com", "TEST3@example.com"],
+            ["test4@example.com", "test4@example.com"],
         ]
         for email, expected in sample_email:
-            user = get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raised a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'test123')
+            get_user_model().objects.create_user("", "test123")
 
     def test_create_superuser(self):
         """Test creating a supersuer."""
         user = get_user_model().objects.create_superuser(
-            'test@example.com',
-            'test123',
+            "test@example.com",
+            "test123",
         )
 
         self.assertTrue(user.is_superuser)
